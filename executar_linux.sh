d7c50ac4bac1eec0c69402ab803d04f1a4783658
pmarques097-netizen <pmarques097@gmail.com>
2026-07-10 08:59:02 -0300

    Delete executar_linux.sh

--- executar_linux.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"
-python3 -m venv .venv || true
-source .venv/bin/activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-uvicorn backend.app.main:app --host 0.0.0.0 --port 8000
